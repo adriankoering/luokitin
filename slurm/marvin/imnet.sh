290ca71a50d6bad3665fcc4c0cf468d1d9ff15e3
Adrian Köring <308280adriankoering@users.noreply.github.com>
2024-03-20 07:47:02 +0100

    Lengthen training a bit

--- slurm/marvin/imnet.sh
+++ slurm/marvin/imnet.sh
@@ -6,5 +6,5 @@ export WANDB_JOB_NAME=${WANDB_NAME}
 export WANDB_TAGS="initial,imagenet,resnet18,frozen"
 export WANDB_NOTES="Early Devel Run"
 
-export HYDRA_FULL_ERROR=1 
-python train.py trainer=explore experiment=imagenet/baseline
+export HYDRA_FULL_ERROR=1
+python train.py experiment=imagenet/baseline
