daa4bb83f9cb577c2930fb989b226c6c05fa341c
Adrian Köring <308280adriankoering@users.noreply.github.com>
2024-03-26 06:56:38 +0100

    Disabled pretrained encoders

--- README.md
+++ README.md
@@ -9,5 +9,11 @@ Classification and Segmentation with Deep Learning in PyTorch utilizing WandB an
 $ conda env create -f environment.yml
 ```
 
+## Next Steps:
+1. Resnet 18 + Erase for less overfitting
+2. Resnet 18 + Shorter LR Schedule (about half)
+3. Resnet 34 -> see how bad overfitting gets
+4. Try different encoders (mobilenet, convnext, resnext, wide_resnet, ...) and compare accuracy at steps and training duration
+
 ## Exploration
 1. [SubmitIt!](https://github.com/facebookincubator/submitit) Plugin
--- slurm/marvin/imnet.sh
+++ slurm/marvin/imnet.sh
@@ -1,10 +1,10 @@
 export WANDB_PROJECT=imagenet
 
-export WANDB_NAME=frozen_resnet18
+export WANDB_NAME=short_fresh_random_crop_conv_atto
 export WANDB_JOB_TYPE=initial
 export WANDB_JOB_NAME=${WANDB_NAME}
-export WANDB_TAGS="initial,imagenet,resnet18,frozen"
+export WANDB_TAGS="initial,imagenet,conv_atto,random_crop,nopretraining"
 export WANDB_NOTES="Early Devel Run"
 
 export HYDRA_FULL_ERROR=1
-python train.py experiment=imagenet/baseline
+python train.py experiment=imagenet/baseline model/encoder=convatto model.encoder.pretrained=False trainer.max_steps=100000
